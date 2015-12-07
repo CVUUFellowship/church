0f6d01be9660041ba0b1dd3c9c992fbaa0634e71
Jake Holland <jakeholland.net@gmail.com>
2015-12-07 02:05:07 -0800

    Pull from live site post-wordpress, post-php-5.7 update, includes Mike's fix for https://github.com/CVUUFellowship/church/issues/7

---
+++ website/public_html/milonic/menu_data_main_newsandnot.js
@@ -0,0 +1,12 @@
+<!-- The CVUUF Milonic license number is 189800 -->
+with(milonic=new menuname("Main Menu")){
+style=menuStyle;
+screenposition="center";
+top=124;
+align="center";
+itemwidth="120px";
+itemheight="30px";
+alwaysvisible=1;
+orientation="horizontal";
+aI("text=*** NEW MENU ITEM ***;showmenu=;");
+}
